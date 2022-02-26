6a65a4ee9f9bd9c7d09a19caad4126fcf542365f
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-02-26 18:44:59 -0500

    create helper function getInterview in selectors.js

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,11 @@
+import Appointment from "components/Appointment";
+
 export const getAppointmentsForDay = (state, day) => {
   if (state.days.length === 0) {
     return [];
   }
 
-  const filteredNames = state.days.filter(stateDay =>     stateDay.name === day     
+  const filteredNames = state.days.filter(stateDay => stateDay.name === day     
   ).map((filterItem) => filterItem.appointments )
 
   if(filteredNames.length === 0) {
@@ -11,4 +13,24 @@ export const getAppointmentsForDay = (state, day) => {
   }
   const appointments = filteredNames[0].map((appointmentId) => state.appointments[appointmentId])  
   return appointments;
+}
+
+
+
+export const getInterview = (state, interview) => {
+
+  if (!interview) {
+    return null;
+  }
+
+  const interviewers = state.interviewers;
+  const interviewerId = interview.interviewer
+
+  const interviewer = interviewers[interviewerId]
+  const result = {
+    student: interview.student,
+    interviewer: interviewer
+  };  
+  
+  return result;
 }
