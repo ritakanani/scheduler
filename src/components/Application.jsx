4e34b4312362cdfc2c93ed6c26cdb91abd6716c1
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-02-26 11:42:04 -0500

    react state of Application, Button and DayList

---
+++ src/components/Application.jsx
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DayList from "./DayList";
+import Appointment from "./Appointment";
+
+import "components/Application.scss";
+
+// Appointment Data
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
+
+
+export default function Application(props) {
+
+  const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:8001/api/days`;
+    axios.get(url)
+          .then(response => { 
+            console.log(response.data); 
+            setDays(response.data);
+          })
+  }, []);
+
+  const appointment = appointments.map((appointment) =>  {
+    return (
+      <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={appointment.interview} />
+    );
+  })
+    
+
+  return (
+    <main className="layout">
+      <section className="sidebar">
+        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={days}
+            // day={"Monday"}
+            value={day}
+            // setDay={(day) => console.log(day)}
+            onChange={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
+      </section>
+      <section className="schedule">
+        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointment}
+        <Appointment key={"last"} time={"5pm"} />
+
+      </section>
+    </main>
+  );
+}
