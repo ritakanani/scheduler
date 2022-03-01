6296e7930c0b534405e8908bc8ea330cd2a4240e
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-02-28 19:20:19 -0500

    create selector function for Appointment and test pass

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,6 @@
-// import Appointment from "components/Appointment";
-
 export const getAppointmentsForDay = (state, day) => {
+  // console.log("state", state);
+  // console.log("day", day);
   if (state.days.length === 0) {
     return [];
   }
@@ -11,12 +11,13 @@ export const getAppointmentsForDay = (state, day) => {
   if(filteredNames.length === 0) {
     return [];
   }
-  const appointments = filteredNames[0].map((appointmentId) => state.appointments[appointmentId])  
+  const appointments = filteredNames[0].map((appointmentId) => state.appointments[appointmentId])
+    
   return appointments;
 }
 
 
-
+// This function will return an object that contains the interview data if it is passed an object that contains an interviewer
 export const getInterview = (state, interview) => {
 
   if (!interview) {
@@ -30,7 +31,23 @@ export const getInterview = (state, interview) => {
   const result = {
     student: interview.student,
     interviewer: interviewer
-  };  
-  
+  };   
   return result;
 }
+
+export const getInterviewersForDay = (state, day) => {
+  if (state.days.length === 0) {
+    return [];
+  }
+
+  const filteredNames = state.days.filter(stateDay => stateDay.name === day     
+  ).map((filterItem) => filterItem.interviewers )
+
+  if(filteredNames.length === 0) {
+    return [];
+  }
+  const interviewers = filteredNames[0].map((interviewerId) => state.interviewers[interviewerId])  
+  return interviewers;
+}
+
+
